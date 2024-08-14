f93a755d49bcdefe7d910138e04f84b56e2f85d2
kashifulhaque <haque.kashiful7@gmail.com>
2024-08-15 01:17:17 +0530

    Cleaned the code a little bit

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from utils.utils import handle_user_mentions, replace_emojis
 from utils.config import (
     CONTEXT_LIMIT,
     DISCORD_TOKEN,
-    get_time_based_greeting,
     server_lore,
     server_contexts,
 )
@@ -31,25 +30,19 @@ async def on_message(message):
     server_id = message.guild.id
     prompt = str(message.content).strip()
 
-    ### Don't process if the message is empty or by the bot itself
     if len(prompt) == 0 or message.author.bot:
         return
-    
-    ### Make sure the bot only uses the channel "#chat"
+
     if message.channel.name != "chat":
         await message.channel.send("Ping me in #chat to talk")
         return
 
-    ### Reset the chat on trigger phrase
     if "reset chat" in prompt.lower():
         server_contexts[server_id] = []
         await message.channel.send("Context reset! Starting a new conversation.")
         return
 
-    ### Handle user mentions
     prompt = handle_user_mentions(prompt, message)
-
-    ### Build the context for the conversation
     server_contexts[server_id].append(
         {
             "role": "user",
@@ -65,11 +58,8 @@ async def on_message(message):
 
     bot_response = call_model(messages)
     bot_response_with_emojis = replace_emojis(bot_response, bot.custom_emojis)
-
-    ### Add the bot's response to the conversation context
     server_contexts[server_id].append({"role": "assistant", "content": bot_response})
     await message.channel.send(bot_response_with_emojis, reference=message)
-    # await message.channel.send("-# " + str(len(server_contexts[server_id]) // 2) + "/" + str(CONTEXT_LIMIT // 2) + " messages")
 
     ### Reset context if it gets too large
     if len(server_contexts[server_id]) >= CONTEXT_LIMIT:
@@ -77,12 +67,6 @@ async def on_message(message):
         await message.channel.send("Context reset! Starting a new conversation.")
 
     await bot.process_commands(message)
-
-
-@bot.command(name="hello")
-async def greet(ctx):
-    greeting = get_time_based_greeting()
-    await ctx.send(f"{greeting}, {ctx.author.name}! How can I assist you today?")
 
 
 bot.run(DISCORD_TOKEN)
