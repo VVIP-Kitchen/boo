9f6a9ab0da69ed53d1e0edbf086cb09e60fd8921
strikernd <143270070+21f3002397@users.noreply.github.com>
2024-08-18 12:20:19 +0530

    Update events.py

--- bot/events.py
+++ bot/events.py
@@ -68,7 +68,8 @@ class BotEvents(commands.Cog):
       if (
         not (is_direct_reply or is_mention) or message.channel.name != self.channel_name
       ):
-        await self.bot.get_context(message).send("Ping me in <#1272840978277072918> to talk", ephemeral = True, reference=message)
+        ctx = await self.bot.get_context(message)
+        ctx..send("Ping me in <#1272840978277072918> to talk", ephemeral = True, reference=message)
         return
 
     if "reset chat" in prompt.lower():
