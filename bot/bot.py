6a3b9b5817bd2ee6fcf44160dfeb89926fd68571
strikernd <143270070+21f3002397@users.noreply.github.com>
2024-08-29 17:57:15 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -16,7 +16,14 @@ class DiscordBot(commands.Bot):
     intents = discord.Intents.default()
     intents.members = True
     intents.message_content = True
-    super().__init__(command_prefix=PREFIX, intents=intents, owner = None, owners=set(ADMIN_LIST))
+    super().__init__(command_prefix=PREFIX, intents=intents, owners=set(ADMIN_LIST)) 
+
+   async def is_owner(self, user: discord.User):
+      if user.id in self.owners:
+        return True
+
+      # Else fall back to the original
+      return await super().is_owner(user)
 
   async def setup_hook(self) -> None:
     """
