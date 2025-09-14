3eb6d2b8c853da2b8f07cf880ceb4a9bd23fcafa
Neelotpal Dutta <143270070+21f3002397@users.noreply.github.com>
2025-09-14 20:17:51 +0530

    Change behavior for Booette detection
    Send a hardcoded message when Booette is detected instead of logging an error.

--- src/utils/message_utils.py
+++ src/utils/message_utils.py
@@ -25,7 +25,7 @@ db_service = DBService()
 def should_ignore(message: Message, bot: commands.Bot) -> str | bool:
   ### Ignore bots - this should always return True to ignore
   if message.author.id == 1413943952524054550:
-    logger.error(f"Booette detected, also bot.user {bot.user}") #info logs are not tracked, so temporarily setting it to error log
+    message.channel.send(f"Hi Booette, this is a hardcoded testing message")
   
   if message.author.bot and not message.author.id == 1413943952524054550:
     return True
